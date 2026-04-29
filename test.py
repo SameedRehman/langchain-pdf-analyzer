068db0164e2ee3ce854082481656b2ca9a9214f1
Sameed Rehman <sameedansari49@gmail.com>
2026-04-29 18:58:06 +0500

    second commit: asking question more then one

--- test.py
+++ test.py
@@ -20,22 +20,32 @@ def load_any_file(file_path):
     return loader.load()
 
 loader = TextLoader("The history of monsters.txt")
-docs = loader.load()
-print(docs[0].page_content)
+docs = load_any_file("/home/sameed/langchain_project/The history of monsters.txt")
+# print(docs[0].page_content)
+monster_text = docs[0].page_content
+
+
 
 pdf_loader = PyPDFLoader("/home/sameed/Downloads/Atomic_Habits_100_Page_Mimic.pdf")
-pdf_docs = pdf_loader.load()
+pdf_docs = load_any_file("/home/sameed/Downloads/Atomic_Habits_100_Page_Mimic.pdf")
 
 print(f"Loaded {len(pdf_docs)} pages from the document")
+pdf_text = pdf_docs[0].page_content + pdf_docs[1].page_content
 
-context_text = pdf_docs[0].page_content + pdf_docs[1].page_content
+context_text = monster_text +"\n\n----NEW DOCUMENT---\n\n" + pdf_text
 
 # response = llm.invoke(prompt,"what is coding")
+
+combined_question = """
+1. What is the main point of the 'Atomic Habits' section?
+2. Write a 5-line conclusion summarizing the 'History of Monsters' section.
+"""
 
 chain = prompt | llm  | output_parser
 
 result = chain.invoke({
     "context":context_text,
-    "question": "What is the main point of the document"})
+    "question": combined_question
+    })
 
 print(result)
